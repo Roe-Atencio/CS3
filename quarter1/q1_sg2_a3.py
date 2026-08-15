6f0bfefc9682b79d777e5a1d465e47b4a241d53a
Roe-Atencio <clatencio2030@clc.pshs.edu.ph>
2026-08-15 11:15:08 +0800

    Add Chinese Zodiac sign calculator

--- quarter1/q1_sg2_a3.py
+++ quarter1/q1_sg2_a3.py
@@ -1,1 +1,16 @@
+# Section: 9 - Pinatubo                                 Score:____________
+# C# / Name: #16 Umayan, #17 Aguilar, #18 Atencio       Date: 08/15/2026
 
+birth_year = int(input("Enter your birth year: "))
+zodiac_animals = ["Monkey (猴 / Hóu)", "Rooster (鸡 / Jī)", "Dog (狗 / Gǒu)", "Pig (猪 / Zhū)", "Rat (鼠 / Shǔ)", "Ox (牛 / Niú)", 
+                  "Tiger (虎 / Hǔ)", "Rabbit (兔 / Tù)", "Dragon (龙 / Lóng)", "Snake (蛇 / Shé)", "Horse (马 / Mǎ)", "Goat (羊 / Yáng)"]
+def get_zodiac(birth_year, zodiac_animals):
+  index = int(birth_year%12)
+  Chinese_zodiac = zodiac_animals[index]
+  return Chinese_zodiac
+
+if birth_year >= 1900:
+  Chinese_zodiac = get_zodiac(birth_year, zodiac_animals)
+  print(f"Your Chinese Zodiac Sign is: {Chinese_zodiac}")
+else:
+  print("Invalid year. It should not be earlier than 1900.")
