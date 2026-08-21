a1d9e1c0d42463b11ebc0621a46802475c18a5a8
Roe-Atencio <clatencio2030@clc.pshs.edu.ph>
2026-08-21 13:39:44 +0800

    Add Product class and product details display

--- quarter1/practice_coding.py
+++ quarter1/practice_coding.py
@@ -1,1 +1,17 @@
+class Product:
+  def __init__(self, name, price):
+    self.name = name
+    self.price = float(price)
+  def product_details(self):
+    print(f"Product Details:\nProduct Name: {self.name}\nPrice: {self.price}")
 
+Products = [ ]
+
+for i in range(5):
+  name = input("Enter product name: ")
+  price = float(input("Enter price of product: "))
+  name = Product(name, price)
+  name.product_details()
+  Products.append(name)
+
+print(Products)
