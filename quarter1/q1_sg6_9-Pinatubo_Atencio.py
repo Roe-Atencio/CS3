6f4a754090811472510882a5918919065848e3a5
Roe-Atencio <clatencio2030@clc.pshs.edu.ph>
2026-09-10 10:40:45 +0800

    Update q1_sg6_9-Pinatubo_Atencio.py

--- quarter1/q1_sg6_9-Pinatubo_Atencio.py
+++ quarter1/q1_sg6_9-Pinatubo_Atencio.py
@@ -1,3 +1,18 @@
 # Section: 9 - Pinatubo        Score:____________
 # C# / Name: #18 Atencio       Date: 08/15/2026
 
+class Lab:
+  def __init__(self, room_number):
+    self.room_number = room_number
+
+class Technician:
+  def __init__(self, name):
+    self.name = name
+  def assigned_lab(self, lab_obj):
+    self.assigned_lab = lab_obj
+
+chem_lab = Lab("302")
+mr_cruz = Technician("Mr. Cruz")
+mr_cruz.assign_lab(chem_lab)
+
+print(mr_cruz.assigned_lab.room_number)
