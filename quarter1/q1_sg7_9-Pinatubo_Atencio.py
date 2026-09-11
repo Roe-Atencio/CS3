d96e064d65abaa033bdc950a0d2ca056ead40133
Roe-Atencio <clatencio2030@clc.pshs.edu.ph>
2026-09-11 18:40:47 +0800

    Refactor Glassware and Beaker classes with names

--- quarter1/q1_sg7_9-Pinatubo_Atencio.py
+++ quarter1/q1_sg7_9-Pinatubo_Atencio.py
@@ -2,16 +2,35 @@
 # C# / Name: #18 Atencio       Date: 08/15/2026
 
 class Glassware:
-  def __init__(self, glassware_type):
-    self.glassware_type = glassware_type
-    print("Glassware removed from cabinet.")
-  def __del__(self):
-    print(glassware_type, "shattered on the floor.")
+    def __init__(self, name, glassware_type):
+        self.name = name
+        self.glassware_type = glassware_type
+        print(f"{name} removed from cabinet.")
+
+    def __del__(self):
+        print(f"{self.name} shattered on the floor.")
 
 class Beaker(Glassware):
-  def __init__(self, glassware_type, liquid_amount):
-    super().__init__(glassware_type)
-    self.liquid_amount = liquid_amount
+    def __init__(self, name, glassware_type, volume):
+        super().__init__(name, glassware_type)
+        self.volume = volume
+        print(f"{name} is holding {volume} mL of liquid.")
+
 class Tray:
-  def __init__(self):
-    print("Tray removed from cabinet.")
+    def __init__(self, name, beakers):
+        self.name = name
+        print(f"{name} removed from cabinet.")
+        self.beakers = beakers
+
+    def __del__(self):
+        print(f"{self.name} accidentally fell on the floor.")
+        self.beakers = []
+
+beaker_list = [Beaker("Beaker 1", "Beaker", 10),
+              Beaker("Beaker 2", "Beaker", 20),
+              Beaker("Beaker 3", "Beaker", 30),
+              Beaker("Beaker 4", "Beaker", 40),
+              Beaker("Beaker 5", "Beaker", 50),]
+
+tray = Tray("tray", beaker_list)
+del tray
