e3e784e9bffbfdb651db75e1dd207a9161577eb7
Roe-Atencio <clatencio2030@clc.pshs.edu.ph>
2026-09-18 08:59:29 +0800

    Implement Sauce and Tusoktusok classes
    Added Sauce and Tusoktusok classes with dip and eat methods.

--- quarter1/Tusoktusok.py
+++ quarter1/Tusoktusok.py
@@ -1,1 +1,22 @@
+class Sauce:
+  def __init__(self, name, taste):
+    self.name = name
+    self.taste = taste
 
+class Tusoktusok:
+  name = ""
+  sauce = []
+  def __init__(self, name):
+    self.name = name
+  def dip(self, sauce):
+    self.sauce.append(sauce)
+  def eat(self):
+    print(f"I ate {self.name} with {self.sauce.name} and it tastes {self.sauce.taste}.")
+
+fishball = Tusoktusok("fishball")
+vinegar = Sauce("vinegar", "sour")
+sweet = Sauce("sweet sauce", "sweet")
+spicy = ("spicy sauce", "spicy")
+fishball.dip(vinegar)
+fishball.dip(sweet)
+fishball.dip(spicy)
